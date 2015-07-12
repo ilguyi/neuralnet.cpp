3e9d39a89c686c9985270aa15be7ba3e1dcb7cf4
Il Gu Yi <gubs@Il-Gu-Yiui-MacBook-Pro-Retina.local>
2015-07-13 00:05:46 +0900

    add parameters file

---
+++ parameters.d
@@ -0,0 +1,31 @@
+// Reading Parameters setting file
+// DO NOT ERASE ALL ANNOTATION
+// Number of training data
+180000
+// Number of input data dimension
+784
+// Number of validation data
+30000
+// Number of hidden nodes in each hidden layer n_h1, n_h2, n_h3, ...
+500
+// Number of class
+10
+// Learning rate
+0.3
+// Cost function type (CrossEntropy or Quadratic)
+//	using quadratic cost function C = (target - activation)^2 / 2
+//	using cross entropy cost function
+//	C = target * log activation + (1 - target) * log (1 - activation) 
+Quadratic
+// Regularization parameter
+0.0
+// Mini batch size
+10
+// Sigmoid type (Binary or Bipolar)
+Binary
+// sigmoid_alpha
+1.0
+// iteration max step
+1
+// Initialize type (gaussian or uniform)
+gaussian
