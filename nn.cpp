3d8a017da4f96652f11021bf1ed61e34db5df92e
Il Gu Yi <gubs@Il-Gu-Yiui-MacBook-Pro-Retina.local>
2015-09-22 13:22:53 +0900

    nn.cpp

--- nn.cpp
+++ nn.cpp
@@ -25,49 +25,43 @@ int main (int argc, char** argv)
     nn.ReadParameters(argv[3]);
     nn.PrintParameters();
 
-    unsigned N = nn.GetN_train() + nn.GetN_valid();
+    unsigned N = nn.GetN_train() + nn.GetN_valid() + nn.GetN_test();
+//    unsigned N = nn.GetN_train() + nn.GetN_valid();
     unsigned dimension = nn.GetDimension();
-
 
     //  Reading training data
     DataFrame<unsigned> trainMNIST;
 //  Usage: ReadDataFile(filename, N, dimension, header, target)
-    trainMNIST.ReadDataFile(argv[1], N, dimension, "True", "True");
+    trainMNIST.ReadDataFile(argv[1], N, dimension, true, true);
     cout << "Read training data complete" << endl;
-//  trainMNIST.PrintData();
 
     //  Set target data
-    arma::uvec target_class;
-    target_class << 0 << 1 << 2 << 3 << 4 << 5 << 6 << 7 << 8 << 9;
-    if ( target_class.size() != nn.GetN_class() ) {
-        cout << "Usage: number of class is NOT the same" << endl;
-        exit(1);
-    }
-    trainMNIST.SetTargetMatrix(target_class, df::Binary);
+    trainMNIST.SetTargetMatrix(nn.GetTargetClass(), "Binary");
     cout << "Target Matrix complete" << endl;
-//  trainMNIST.PrintTarget();
-//  trainMNIST.PrintTargetMatrix();
 
 
     //  Randomly extract validation data
-    DataFrame<unsigned> validMNIST;
-    trainMNIST.SplitValidationSet(validMNIST, nn.GetN_valid());
-    cout << "Split validation data complete" << endl;
+//    DataFrame<unsigned> validMNIST;
+//    trainMNIST.SplitValidationSet(validMNIST, nn.GetN_valid());
+//    cout << "Split validation data complete" << endl;
+
+    //  Randomly extract validation data and test data
+    DataFrame<unsigned> validMNIST, testMNIST;
+    trainMNIST.SplitValidTestSet(validMNIST, nn.GetN_valid(), testMNIST, nn.GetN_test());
+    cout << "Split validation data and test data complete" << endl;
 
 
     //  Reading test data
-    DataFrame<unsigned> testMNIST;
-    unsigned N_test = nn.GetN_test();
-    testMNIST.ReadDataFile(argv[2], N_test, dimension, "True", "False");
-    cout << "Read test data complete" << endl;
-//  testMNIST.PrintData();
+//    DataFrame<unsigned> testMNIST;
+//    unsigned N_test = nn.GetN_test();
+//    testMNIST.ReadDataFile(argv[2], N_test, dimension, true, false);
+//    cout << "Read test data complete" << endl;
 
 
     //  Set linear scaling each features
     DataFrame<double> trainMNIST_norm, validMNIST_norm, testMNIST_norm;
-//    trainMNIST.LinearScalingEachFeatures(validMNIST, testMNIST, trainMNIST_norm, validMNIST_norm, testMNIST_norm);
     trainMNIST.NormalizationEachFeatures(validMNIST, testMNIST, trainMNIST_norm, validMNIST_norm, testMNIST_norm);
-    cout << "Linear scaling all data complete" << endl;
+    cout << "Normalization all data complete" << endl;
 //    trainMNIST_norm.PrintData();
 //    validMNIST_norm.PrintData();
 //    testMNIST_norm.PrintData();
@@ -76,18 +70,16 @@ int main (int argc, char** argv)
 
 //  Execute NeuralNetworks
     nn.Initialize();
-    cout << "before training" << endl;
-    nn.PrintResults();
 
     for (unsigned iter=0; iter<1; iter++) {
 
 //        nn.Training(trainMNIST_norm, iter);
-        nn.Training(trainMNIST_norm, validMNIST_norm, iter);
+//        nn.Training(trainMNIST_norm, validMNIST_norm, iter);
+        nn.Training(trainMNIST_norm, validMNIST_norm, testMNIST_norm, iter);
 
-        arma::uvec predict(N_test);
-        predict.zeros();
-
-        nn.Test(testMNIST_norm, predict, iter);
+//        arma::uvec predict(N_test);
+//        predict.zeros();
+//        nn.Test(testMNIST_norm, predict, iter);
     }
 
 
