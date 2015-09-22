8a545ed25489bd3dd783259be9ba4434b1a4f2f9
Il Gu Yi <gubs@Il-Gu-Yiui-MacBook-Pro-Retina.local>
2015-09-22 13:20:23 +0900

    parameters.d

--- parameters.d
+++ parameters.d
@@ -1,17 +1,17 @@
 // Reading Parameters setting file
 // DO NOT ERASE ALL ANNOTATION
 // Number of training data
-270
+250
 // Number of input data dimension
 784
 // Number of validation data
 30
 // Number of test data
-30
+20
 // Number of hidden nodes in each hidden layer n_h1, n_h2, n_h3, ...
-15
-// Number of class
-10
+100
+// Target class
+0 1 2 3 4 5 6 7 8 9
 // Learning rate
 0.003
 // Cost function type (CrossEntropy or Quadratic)
@@ -24,14 +24,8 @@ CrossEntropy
 // Momentum parameter
 0.9
 // Mini batch size
-50
+20
 // Softmax option at output nodes (True or False)
 True
-// Sigmoid type (Binary or Bipolar)
-Binary
-// sigmoid_alpha
-1.0
 // iteration max epochs
-1
-// Initialize type (gaussian or uniform)
-gaussian
+20
